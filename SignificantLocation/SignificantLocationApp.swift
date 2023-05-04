ba006ee4d1490d7d79dca6cba3fabb7db307d8c3
Joao Macedo <joao.macedo@ftdata.com.br>
2023-05-04 00:50:34 -0300

    Initial Commit

---
+++ SignificantLocation/SignificantLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignificantLocationApp.swift
+//  SignificantLocation
+//
+//  Created by João Macedo on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SignificantLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
